538ed957b1a97bef06618cc2fe7a93a1ac9143e0
Loucami <82464572+Loucami@users.noreply.github.com>
2023-05-24 17:09:35 +0200

    Delete src directory

--- src/data/fruits.js
+++
@@ -1,9 +0,0 @@
-import Fruit from '../models/Fruit';
-
-const fruits = [
-    new Fruit("Citron","#FFCC00"),
-    new Fruit("Peche", "#F44336"),
-    new Fruit("Fraise", "#E53935"),
-];
-
-export default fruits;
